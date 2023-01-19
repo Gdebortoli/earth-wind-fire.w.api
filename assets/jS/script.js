348a8e2546b09f6b392418d99c1e8a31a7168df6
“Gigi <debortolg@gmail.com>
2023-01-18 20:28:52 -0800

    city search functionality - added api calls

--- assets/jS/script.js
+++ assets/jS/script.js
@@ -13,10 +13,31 @@ let prevSearch = document.querySelector('#past-searches');
 let searchedArray = [];
 
 // City Search // Search Btn 
+const searchWeatherForm = (event) => {
+    event.preventDefault();
+    var currCity = citySearch.ariaValueMax.trim(); // for formatting 
+    if (currCity) {
+    cityName.value = "";
+    const apiCall = `api.openweathermap.org/data/2.5/forecast?q=${currCity}&appid=${apiKey}`;
+    //fetch req for city
+    fetch(apiCall)
+    .then(data => data.json())
+    .then(data => {
+    let { lat, lon } = data.coordinates; 
+    // original api call needed to get city data
+    let wfApi = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    // fetch pt 2
+    fetch(wfApi)
+    .then(response => {
+        return response.json(); 
+    })
 
+    })
+    }
+}
 
 // Current weather 
-
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 // 5 day forecast 
 
